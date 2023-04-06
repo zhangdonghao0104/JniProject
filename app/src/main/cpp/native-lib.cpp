00cc4fd45a0b506611ee29183df050590dbadd90
飞白婉儿 <zdh@zenx.ltd>
2023-04-06 16:33:55 +0800

    操作数组

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,65 @@
 #include <jni.h>
 #include <string>
+#include <android/log.h>
 
+#define LOG_TAG "CPPLOG"
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#define LOG_TAG "CPPLOG"
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_zdh_jni_MainActivity_stringFromJNI(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+}
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_zdh_jni_MainActivity_changeName(JNIEnv *env, jobject thiz) {
+    // TODO: implement changeName()
+
+    jclass mainActivityCls = env->FindClass("com/zdh/jni/MainActivity");
+
+
+    jfieldID nameFid = env->GetFieldID(mainActivityCls, "name", "Ljava/lang/String;");
+
+    jstring value = env->NewStringUTF("修改为test");
+
+    env->SetObjectField(thiz, nameFid, value);
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_zdh_jni_MainActivity_changeAge(JNIEnv *env, jobject thiz) {
+    jclass mainActivityCls = env->GetObjectClass(thiz);
+    jfieldID ageFid = env->GetFieldID(mainActivityCls, "age", "I");
+    env->SetIntField(thiz, ageFid, 29);
+}
+
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_zdh_jni_MainActivity_testArrayAction(JNIEnv *env,
+                                              jobject thiz,
+                                              jint count,
+                                              jstring text_info,
+                                              jintArray ints,
+                                              jobjectArray strs) {
+    // TODO: implement testArrayAction()
+    int _count = count;
+    LOGD("C++ _count:%d\n", count);
+    const char *_text_info = env->GetStringUTFChars(text_info, NULL);
+    LOGD("C++ _count:%s\n", _text_info);
+    //回收代码
+    env->ReleaseStringUTFChars(text_info, _text_info);
+    jint *_ints = env->GetIntArrayElements(ints, NULL);
+    int intsLen = env->GetArrayLength(ints);
+    for (int i = 0; i < intsLen; ++i) {
+        env->GetIntArrayElements(ints, NULL);
+        //* 取出指针的值 挪动指针，遍历指针就是遍历数组
+        LOGI("C++ ints item:%d\n", *(_ints + i));
+    }
+
+
 }
--- app/src/main/java/com/zdh/jni/MainActivity.java
+++ app/src/main/java/com/zdh/jni/MainActivity.java
@@ -3,18 +3,28 @@ package com.zdh.jni;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import com.zdh.jni.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
     }
 
+    public native void changeName();
+
+    public native void changeAge();
+
+    private String name = "zdh";
+    private int age = 10;
+
     private ActivityMainBinding binding;
+
+    public native void testArrayAction(int count, String textInfo, int[] ints, String[] strs); // String引用类型，玩数组
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +33,48 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Example of a call to a native method
         TextView tv = binding.sampleText;
         tv.setText(stringFromJNI());
+
+
+        Log.e("TAG", "onCreate: 修改前" + name);
+        changeName();
+        Log.e("TAG", "onCreate: 修改后" + name);
+
+        Log.e("TAG", "onCreate: 年龄修改前" + age);
+        changeAge();
+        Log.e("TAG", "onCreate: 年龄修改后" + age);
+    }
+
+    // 点击事件：操作insertObject函数
+    public void test01(View view) {
+        int[] ints = new int[]{1, 2, 3, 4, 5, 6};
+        String[] strs = new String[]{"李小龙", "李连杰", "李元霸"};
+        testArrayAction(99, "你好", ints, strs);
+
+        for (int anInt : ints) { // Java 输出 int 数组
+            Log.d("TAG", "Java test01: java ints:" + anInt);
+        }
+
+        for (String str : strs) { // 输出 String 数组
+            Log.e("TAG", "Java test01: java strs:" + str);
+        }
+    }
+
+    // 点击事件：操作insertObject函数
+    public void test02(View view) {
+    }
+
+    // 点击事件：操作insertObject函数
+    public void test03(View view) {
+    }
+
+    // 点击事件：操作insertObject函数
+    public void test04(View view) {
+    }
+
+    // 点击事件：操作insertObject函数
+    public void test05(View view) {
     }
 
     /**
